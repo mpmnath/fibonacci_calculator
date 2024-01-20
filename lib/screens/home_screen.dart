ef4c5974fce4e3c591264a5768617fd9e25c9753
Manjunath Muntha Purushotham <mpm.nath007@gmail.com>
2024-01-20 12:25:21 +0100

    Add Fibonacci Calculator functionality to HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,74 @@
+import 'package:fibonacci_calculator/bloc/fib_calc_bloc.dart';
+import 'package:fibonacci_calculator/bloc/fib_calc_event.dart';
+import 'package:fibonacci_calculator/bloc/fib_calc_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final TextEditingController _inputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Fibonacci Generator"),
+        title: const Text("Fibonacci Calculator"),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                const Text("Input"),
-                const TextField(),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Is Fibonacci Number?"),
-                ),
-                const Divider(
-                  height: 50,
-                ),
-                const Text("Output"),
-              ],
+            child: BlocBuilder<FibCalcBloc, FibCalcState>(
+              builder: (context, state) => Column(
+                children: [
+                  TextFormField(
+                    controller: _inputController,
+                    maxLength: 100,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Input",
+                    ),
+                    keyboardType: TextInputType.number,
+                    /* validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please input a number";
+                      }
+                      if (int.tryParse(value) == null) {
+                        return "Please enter a valid number";
+                      }
+                      return null;
+                    }, */
+                  ),
+                  const Divider(
+                    height: 50,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final int value = int.parse(_inputController.text);
+                      context
+                          .read<FibCalcBloc>()
+                          .add(CheckFibonacciNumber(value));
+                      _inputController.clear();
+                    },
+                    child: const Text("Is Fibonacci Number?"),
+                  ),
+                  const Divider(
+                    height: 50,
+                  ),
+                  state is FibCalcResult
+                      ? Text(
+                          state.result
+                              ? "The input is a Fibonacci number"
+                              : "The input is not a Fibonacci number",
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : const SizedBox(),
+                ],
+              ),
             ),
           ),
         ),
