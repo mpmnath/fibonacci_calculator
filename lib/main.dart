ebb8cc5e0fe61a307c2fd63c89e00aba51506089
Manjunath Muntha Purushotham <mpm.nath007@gmail.com>
2024-01-20 12:25:13 +0100

    Add Fibonacci Calculator bloc and import Flutter Bloc package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:fibonacci_calculator/bloc/fib_calc_bloc.dart';
 import 'package:fibonacci_calculator/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +13,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (_) => FibCalcBloc(),
+      child: MaterialApp(
+        title: 'Fibonacci Calculator',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: HomeScreen(),
       ),
-      home: const HomeScreen() ,
     );
   }
 }
